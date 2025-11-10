5073f57b22437f84777c7086fe560409c46e6fe6
sahilsol0 <sahilsolodev@gmail.com>
2025-11-10 20:47:45 +0530

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,26 @@ import {motion} from "motion/react"
 import AnimatedPage from "../components/AnimatedPage.jsx"
 
 function Contact() {
+  const socials = [
+    { platform: 'Mail', linkText: 'shreyasolomon25@gmail.com', link: 'mailto:shreyasolomon25@gmail.com'},
+    { platform: 'Whatsapp', linkText: '8696621129', link: 'https://wa.me/+918696621129'}
+    ]
   return (
     <AnimatedPage>
-      <h1 className="text-9xl">Contact Page</h1>
+      <section className="px-4 pt-20">
+        <h1 className="text-5xl font-serif border-b border-primary">Contact me.</h1>
+        <p className="text-sm mt-4">Currently you can only contact me through mail. Don't worry more links will be added in future.</p>
+        <ul className="flex flex-col gap-8 mt-12">
+          {socials.map((social, i)=> (
+            <li key={i} 
+              className="flex flex-col bg-primary/0 rounded"
+            >
+              <h1>{social.platform}</h1>
+              <a href={social.link} target="redirect" className="text-primary border-b">{social.linkText}</a>
+            </li>
+          ))}
+        </ul>
+      </section>
     </AnimatedPage>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,10 @@ function Home() {
           <HeroArt />
         </div>
         <section className="mt-48">
-        <h2 className="text-3xl font-serif border-b border-primary">About Me.</h2>
-        <p className="mt-8">Born to Malayali parents and raised in Rajasthan, I was exposed to the cultures of both North and South India<span className="text-primary">*</span>.<br /><span className="text-[0.6rem] text-primary">[*Generally, these two don't get along much, but here I am]</span></p>
+        <h2 className="text-5xl font-serif border-b border-primary">About Me.</h2>
+        <p className="mt-8">Born to Malayali parents and raised in Rajasthan, I was exposed to the cultures of both North and South India<span className="text-primary">*</span>.<br /><span className="text-[0.7rem] text-primary">[*Generally, these two don't get along much, but here I am]</span></p>
         <p className="mt-8">While I spoke fluent Hindi and had mostly North Indian friends, my parents ensured that I never lost touch with my roots (being a thani naadan Malayali !). Maybe that's why after completing my schooling(12th) in Rajasthan, I wanted Kerala to be the next step in my journey.</p>
-        <p className="mt-6">So I wrote the entrance exam for architecture, qualified, but I knew, I could do better. So, I took a gap year, prepared and tried again. This time the stars aligned just perfectly! I got a college that I liked and now I'm in first year of B.Arch doing architecture things<span><span className="text-primary">*</span></span>.<br/><span className="text-[0.6rem] text-primary">[*Which mostly involves losing sleep over assignments you know your professor will ask you to redo for the 750th time.]</span></p>
+        <p className="mt-6">So I wrote the entrance exam for architecture, qualified, but I knew, I could do better. So, I took a gap year, prepared and tried again. This time the stars aligned just perfectly! I got a college that I liked and now I'm in first year of B.Arch doing architecture things<span><span className="text-primary">*</span></span>.<br/><span className="text-[0.7rem] text-primary line-height-1">[*Which mostly involves losing sleep over assignments you know your professor will ask you to redo for the 750th time.]</span></p>
         <div className="h-screen flex items-center">
           <p className="font-serif text-2xl">"This exposure to cultures has shaped how I study and approach architecture today. To me, a good design is one that elevates a place not merely through aesthetics, but also by respecting and empowering the people and their culture."</p>
         </div>
